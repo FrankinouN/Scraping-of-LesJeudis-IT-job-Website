347a2dd9b983f7400464a93adcceef3a1c587b4a
FrankinouN <48446916+FrankinouN@users.noreply.github.com>
2019-06-17 18:22:25 +0200

    Add files via upload

--- scrapingLesJeudis.py
+++ scrapingLesJeudis.py
@@ -1,12 +1,14 @@
 
 
+# importation de la bibliotheque scrapy et parametrage de la classe BlogSpider
+
 import scrapy
 
 class BlogSpider(scrapy.Spider):
 	name = 'blogspider'
 
 	start_urls = ['https://www.lesjeudis.com/recherche?utf8=✓&q=data+analyst&loc=Île-de-France']
-
+        # definition des features qui seront scrapées pour chaque offre d'emploi
 	def parse(self, response):
 		for rubrique in response.css('div.job-info'):
 			yield {
@@ -17,6 +19,7 @@ class BlogSpider(scrapy.Spider):
 					'Type de poste':rubrique.css('div.snapshot-item ::text').getall()
 					}
 
+		# chargement des pages pages suivantes			
 		for next_page in response.css('a.btn-arrow::attr(href)').getall():
 			print(next_page)
 			if next_page is not None:
@@ -25,4 +28,6 @@ class BlogSpider(scrapy.Spider):
 
 
 
+
+# commande à utiliser pour lancer le scraping 
 # scrapy runspider scrapingLesJeudis.py -o scrapp.json
